70072d2636f7bb9609f3058dd98ce48fae2a36bd
Steven-Debande <51061507+Steven-Debande@users.noreply.github.com>
2019-05-30 06:32:46 +0200

    Update start.js

--- start.js
+++ start.js
@@ -1,32 +1,21 @@
-const folderDir = "./drop the files";
+const folderDir = "./drop the files/";
 
-const { readdir, rename } = require("fs");
-const Name = process.argv.slice(2)[0].replace(/:/g, " ");
-const Ignored = process.argv.slice(3).join(" ").split(":");
+const { readdirSync, renameSync } = require("fs");
+const NAME = process.argv.slice(2)[0].replace(/:/g, " ");
+const IGNORE = process.argv.slice(3).join(" ").split(":");
 
-let count = 0;
+let COUNT_FILES = 0;
+const folder = readdirSync(folderDir);
 
-readdir(folderDir, async(err, files) => {
-    if (err) {
-        return console.log(`[RenameFiles]: ${err}`);
-        process.exit(1);
+folder.forEach((item) => {
+    const oldName = item.split(".")[0];
+    const extendFile = item.split(".")[0];
+
+    if (!IGNORE.includes(extendFile)) {
+        COUNT_FILES++
+        const newName = `${NAME}_${COUNT_FILES}`;
+        renameSync(folderDir+item, folderDir+item.replace(oldName, newName));
     }
-    console.log(`[RenameFiles]: ${files.length} files found.`);
-    console.log(`[RenameFiles]: Extends ignored [${Ignored.map(i => `"${i}"`)}]`);
-    files.forEach(file => {
-        const oldName = file.split(".")[0];
-        const extendFile = file.split(".")[1];
-
-        if (!Ignored.includes(extendFile)) {
-            count++;
-            const newName = `${Name}_${count}`;
-            rename(`${folderDir}/${file}`, `${folderDir}/${file.replace(oldName, newName)}`, (err) => {
-                if (err) {
-                    return console.log(`[RenameFiles]: ${err}`);
-                }
-                console.log(`[RenameFiles]: ${oldName} => ${newName}`);
-            });
-        }
-    });
-    //console.log(`[RenameFiles]: ${count} files rename!`);
 });
+
+console.log(`${COUNT_FILES} rename`);
